685c59260c26dd674344c43393516902f5746018
snip-animesh <animesh12126@gmail.com>
2022-06-22 14:42:14 +0600

    Hand tracking added

--- Feature_Detection/ImageClassifierFeatureDetecors.py
+++ Feature_Detection/ImageClassifierFeatureDetecors.py
@@ -2,36 +2,6 @@ import cv2
 import numpy as np
 import os
 
-# img1=cv2.imread("ImageQuery/ps4fifa.jpg",0)
-# img2=cv2.imread("ImageQuery/ps4fifa2.jpg",0)
-#
-# orb=cv2.ORB_create(nfeatures=1000)
-#
-# kp1,des1=orb.detectAndCompute(img1,None)
-# kp2,des2=orb.detectAndCompute(img2,None)
-
-# imgKp1=cv2.drawKeypoints(img1,kp1,None)
-# imgKp2=cv2.drawKeypoints(img2,kp1,None)
-
-# bf=cv2.BFMatcher()
-# matches=bf.knnMatch(des1,des2,k=2)
-#
-# good=[]
-# for m,n in matches:
-#     if m.distance <0.75* n.distance:
-#         good.append([m])
-#
-# print(len(good))
-#
-# img3=cv2.drawMatchesKnn(img1,kp1,img2,kp2,good,None,flags=2)
-#
-# cv2.imshow("Image1",img1)
-# cv2.imshow("Image2",img2)
-# cv2.imshow("Image3",img3)
-
-# cv2.imshow("ImageKp1",imgKp1)
-# cv2.imshow("ImageKp2",imgKp2)
-
 PATH = 'ImageQuery'
 ORB = cv2.ORB_create(nfeatures=1000)
 
@@ -93,8 +63,8 @@ while True:
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
     id = findIDs(img2, desLst)
-    if id !=-1:
-        cv2.putText(imgOrginal,classNames[id],(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
+    if id != -1:
+        cv2.putText(imgOrginal, classNames[id], (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
 
     cv2.imshow("WebCam", imgOrginal)
 
---
+++ Hand Tracking/HandTrackingMin.py
@@ -0,0 +1,38 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(1)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+
+while True:
+    succ, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = hands.process(imgRGB)
+    # print(result.multi_hand_landmarks)
+
+    if result.multi_hand_landmarks:
+        for handLmarks in result.multi_hand_landmarks:
+            for id, lm in enumerate(handLmarks.landmark):
+                # print(id,lm)
+                height, width, channel = img.shape
+                cx, cy = int(lm.x * width), int(lm.y * height)  # position of center
+                print(id, cx, cy)
+
+                if id == 0:
+                    cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+
+            mpDraw.draw_landmarks(img, handLmarks, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 255), 3)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
