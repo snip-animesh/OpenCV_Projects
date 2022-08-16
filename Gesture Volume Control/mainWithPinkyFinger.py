d58f21311964c85cab773d8e2972c66ba833f063
snip-animesh <animesh12126@gmail.com>
2022-08-17 03:17:20 +0600

    controlling with pinky finger

--- Gesture Volume Control/mainWithPinkyFinger.py
+++ Gesture Volume Control/mainWithPinkyFinger.py
@@ -12,7 +12,7 @@ from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 # Camera height and width
 wCam, hCam = 640, 480
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(3, wCam)
 cap.set(4, hCam)
 
